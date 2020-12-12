c17e00334c0e31a9a2ece7ab9c93ba2fa8923fde
akrish4 <ananthakrishnan20121999@gmail.com>
2020-12-12 17:17:15 +0530

    updaed commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# madlibs_randomly_fun
+=======
+# madlibs_randomly_fun
+>>>>>>> f3adff58fdaa6b0e3756941607054fb579b113de
---
+++ madlibs.py
@@ -0,0 +1,29 @@
+"""
+Very Beginner Python Project by Kylie Ying
+Madlibs using string concatenation
+YouTube Kylie Ying: https://www.youtube.com/ycubed 
+Twitch KylieYing: https://www.twitch.tv/kylieying 
+Twitter @kylieyying: https://twitter.com/kylieyying 
+Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
+Website: https://www.kylieying.com
+Github: https://www.github.com/kying18 
+Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
+"""
+# # string concatenation (aka how to put strings together)
+# # suppose we want to create a string that says "subscribe to _____ "
+# youtuber = "Kylie Ying" # some string variable
+
+# # a few ways to do this
+# print("subscribe to " + youtuber)
+# print("subscribe to {}".format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
